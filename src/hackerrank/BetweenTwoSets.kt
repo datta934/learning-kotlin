82bf0393481228bb99bd9596d4e22f62720ace97
Mauaa <mauaa.datta@gmail.com>
2020-12-28 10:09:39 +0530

    New Programs

---
+++ src/hackerrank/BetweenTwoSets.kt
@@ -0,0 +1,73 @@
+package hackerrank
+
+import java.io.*
+import java.math.*
+import java.security.*
+import java.text.*
+import java.util.*
+import java.util.concurrent.*
+import java.util.function.*
+import java.util.regex.*
+import java.util.stream.*
+import kotlin.collections.*
+import kotlin.comparisons.*
+import kotlin.io.*
+import kotlin.jvm.*
+import kotlin.jvm.functions.*
+import kotlin.jvm.internal.*
+import kotlin.ranges.*
+import kotlin.sequences.*
+import kotlin.text.*
+
+/*
+ * Complete the 'getTotalX' function below.
+ *
+ * The function is expected to return an INTEGER.
+ * The function accepts following parameters:
+ *  1. INTEGER_ARRAY a
+ *  2. INTEGER_ARRAY b
+ */
+
+fun getTotalX(a: Array<Int>, b: Array<Int>): Int {
+    // Write your code here
+    var total = 0
+    for (x in 1..100) {
+        var status = true
+        for (i in 0 until b.size) {
+            val it = b[i]
+            if (it % x != 0) {
+                status = false
+                break
+            }
+        }
+        if (status) {
+            for (i in 0 until a.size) {
+                val it = a[i]
+                if (x % it != 0) {
+                    status = false
+                    break
+                }
+            }
+        }
+        if (status) {
+            total++
+        }
+    }
+    return total
+}
+
+fun main(args: Array<String>) {
+    val first_multiple_input = readLine()!!.trimEnd().split(" ")
+
+    val n = first_multiple_input[0].toInt()
+
+    val m = first_multiple_input[1].toInt()
+
+    val arr = readLine()!!.trimEnd().split(" ").map{ it.toInt() }.toTypedArray()
+
+    val brr = readLine()!!.trimEnd().split(" ").map{ it.toInt() }.toTypedArray()
+
+    val total = getTotalX(arr, brr)
+
+    println(total)
+}
